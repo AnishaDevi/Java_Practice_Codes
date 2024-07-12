83b92e1fa23e482cd799f96d3c42ef5825749d98
AnishaDevi <anishasdevi444@gmail.com>
2024-07-12 23:49:23 +0530

    "Reading from Console using System.console()"

---
+++ src/ReadingInputConsole/ReadCons.java
@@ -0,0 +1,22 @@
+package ReadingInputConsole;
+//should run this code in terminal java file_path/classname.java
+//here java src/ReadingInputConsole/ReadCons.java
+public class ReadCons
+{
+    public static String getStringFromConsole(int currentYear)
+    {
+        String name = System.console().readLine("Hi , What is your name? ");
+        System.out.println("Thanks "+name);
+
+        String dateOfBirth = System.console().readLine("What year you were born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are "+age+" years old";
+    }
+    public static void main(String[] args)
+    {
+        int currentYear = 2024;
+
+        System.out.println(getStringFromConsole(currentYear));
+    }
+}
